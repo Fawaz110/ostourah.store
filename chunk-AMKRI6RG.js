cab2370f59715033c4ee326b66a05f11e6b2949f
Fawaz110 <mmohamedfawzi23@gmail.com>
2024-11-23 14:10:53 +0200

    uploading

---
+++ chunk-AMKRI6RG.js
@@ -0,0 +1,1 @@
+import{Jb as a,Qb as r,ra as n,tb as o,ub as i}from"./chunk-M2M7UK3E.js";var m=(()=>{class t{static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275cmp=n({type:t,selectors:[["app-not-found"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,d){e&1&&(o(0,"p"),a(1,"not-found works!"),i())}})}}return t})();export{m as NotFoundComponent};

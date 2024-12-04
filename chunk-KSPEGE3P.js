9a5a3e3f0e10f71217212311bbc71b1e5937834f
Fawaz110 <mmohamedfawzi23@gmail.com>
2024-12-04 16:36:52 +0200

    deployment

---
+++ chunk-KSPEGE3P.js
@@ -0,0 +1,1 @@
+import{wa as i}from"./chunk-3KPRBS5G.js";var n=(()=>{class t{transform(r,e){return r.filter(o=>o.category.trim().toLowerCase().includes(e.trim().toLowerCase()))}static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275pipe=i({name:"filterByCategory",type:t,pure:!0,standalone:!0})}}return t})();export{n as a};
